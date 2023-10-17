359961bfd867641126e56564ac00b8fea495849e
hitstrelok <hitstrelok@gmail.com>
2023-10-17 19:52:45 +0100

    No comments

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.math.BigDecimal;
 public class Main {
     public static void main(String[] args) {
 
-        double productNet = 9.99; //77% product cost (100%-23% VAT)
-        double productGross; //100% product cost
+        double productNet = 9.99;
+        double productGross;
 
         System.out.println("Product net: " + productNet);
         productGross =productNet/(1-0.23);
